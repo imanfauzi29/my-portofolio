d46f537a7799f983772508b016852256e21df89b
iman fauzi <imanfauzi29@gmail.com>
2025-02-02 08:28:29 +0700

    feat: create about section

---
+++ src/app/sections/AboutSection.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function AboutSection() {
+  return (
+    <div className="w-full flex flex-col mb-96 justify-center items-center relative">
+      <div className="max-w-3/4 flex items-center gap-20">
+        <div className="break-words sticky self-start top-1/3">
+          <motion.div
+            className="h-full absolute w-2 "
+            animate={{
+              backgroundColor: [
+                "rgb(11, 13, 23)",
+                "rgb(25, 29, 49)",
+                "rgb(48, 35, 74)",
+                "rgb(76, 40, 89)",
+                "rgb(11, 13, 23)",
+              ],
+            }}
+            transition={{
+              duration: 5,
+              repeat: Infinity,
+              repeatType: "reverse",
+              ease: "easeInOut",
+            }}
+          />
+          <h1 className="text-6xl text-white ml-8 leading-tight">
+            {["Turing", "Ideas", "into", "Interface"].map((text) => (
+              <span key={text} className="block">
+                {text}
+              </span>
+            ))}
+          </h1>
+        </div>
+
+        <div className="relative space-y-96 mt-56">
+          <motion.p className="text-white font-kanit text-xl tracking-wide font-light text-balance">
+            Hey! Saya Iman Fauzi M, seorang Frontend Developer yang suka ngulik
+            tampilan web biar gak cuma fungsional, tapi juga enak dipandang.
+            Saya fokus di React.js, Vue.js, dan Next.js, plus suka eksperimen
+            dengan animasi dan UI/UX biar website terasa lebih hidup.
+          </motion.p>
+          <div className="text-white text-xl tracking-wide font-light text-balance">
+            <h2 className=" text-5xl font-medium mb-8">🚀 Kenapa Saya?</h2>
+
+            <ul className="space-y-6">
+              <li>
+                <h5 className="block text-xl font-medium font-kanit">
+                  Frontend Freak
+                </h5>
+                <span className="font-kanit text-gray-300">
+                  Ngoding UI bukan cuma kerjaan, tapi juga hobi.
+                </span>
+              </li>
+              <li>
+                <h5 className="block text-xl font-medium font-kanit">
+                  Pixel Perfect Enthusiast
+                </h5>
+                <span className="font-kanit text-gray-300">
+                  Kalau ada yang miring dikit, auto refactor.
+                </span>
+              </li>
+              <li>
+                <h5 className="block text-xl font-medium font-kanit">
+                  Fast & Optimized
+                </h5>
+                <span className="font-kanit text-gray-300">
+                  Bukan cuma cepat coding, tapi juga performa website harus
+                  kencang! refactor.
+                </span>
+              </li>
+              <li>
+                <h5 className="block text-xl font-medium font-kanit">
+                  Selalu Belajar
+                </h5>
+                <span className="font-kanit text-gray-300">
+                  Lagi ngulik Docker & Wazuh SIEM, siapa tahu nanti jadi
+                  full-stack atau sekalian masuk ke cybersecurity hehe.
+                  refactor.
+                </span>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
